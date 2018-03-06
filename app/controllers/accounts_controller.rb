e6b01cf35baa3d56037f0a2aa41918bf1d3cb67d
Romenig Lima Damasio <romenigld@gmail.com>
2018-03-06 17:37:14 +0100

    Accounts are now linked to owners

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,10 +2,12 @@ class AccountsController < ApplicationController
 
   def new
     @account = Account.new
+    @account.build_owner
   end
 
   def create
     account = Account.create(account_params)
+    sign_in(account.owner)
     flash[:notice] = "Your account has been created."
     redirect_to root_url
   end
@@ -13,6 +15,10 @@ class AccountsController < ApplicationController
   private
 
   def account_params
-    params.require(:account).permit(:name)
+    params.require(:account).permit(:name,
+      { owner_attributes: [
+        :email, :password, :password_confirmation
+      ]}
+    )
   end
 end
