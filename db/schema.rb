c1fbcf497bbed2855623f9e2eb49b329de59f0ca
Romenig Lima Damasio <romenigld@gmail.com>
2018-03-06 19:13:11 +0100

    Added subdomains to accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306162344) do
+ActiveRecord::Schema.define(version: 20180306175107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20180306162344) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "owner_id"
+    t.string   "subdomain"
+    t.index ["subdomain"], name: "index_accounts_on_subdomain", using: :btree
   end
 
   create_table "books", force: :cascade do |t|
--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -6,16 +6,18 @@ feature "Accounts" do
     #save_and_open_page
     click_link "Create a new account"
     fill_in "Name", with: "Test"
+    fill_in "Subdomain", with: "test"
     fill_in "Email", with: "test@example.com"
     fill_in "Password", with: "password", exact: true
     fill_in "Password confirmation", with: "password"
     click_button "Create Account"
 
-    expect(page).to have_content("Signed in as test@example.com")
-
     within(".flash_notice") do
       success_message = "Your account has been created."
       expect(page).to have_content(success_message)
     end
+
+    expect(page).to have_content("Signed in as test@example.com")
+    expect(page.current_url).to eq("http://test.lvh.me/")
   end
 end
