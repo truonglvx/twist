91ad73134f9ef21d37533b85e74845f9030f5056
Romenig Lima Damasio <romenigld@gmail.com>
2018-03-06 20:01:44 +0100

    Added uniqueness validation for subdomain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,3 @@
-# Be sure to restart your server when you modify this file.
-
-#Rails.application.config.session_store :cookie_store, key: '_twist_session'
-
 options = {
   key: "_twist_session"
 }
